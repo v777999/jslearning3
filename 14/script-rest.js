398f0b5f6e86b9bc9926dc5d61a06ca36ef1a176
Viktor <viktor.lugovskoy@yahoo.com>
2022-04-15 18:59:58 +0300

    14 - Rest operator и параметры по умолчанию

---
+++ 14/script-rest.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const log = function (a, b, ...rest) {
+  console.log(a, b, rest);
+};
+
+log("basic", "rest", "operator", "usage"); // basic rest ['operator', 'usage']
+
+function calcOrDouble(number, basis = 2) {
+  console.log(number * basis);
+}
+
+calcOrDouble(3);
+
+
